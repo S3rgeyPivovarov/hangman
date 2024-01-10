bdb757486d891985e9823f609f2d0f375655f95a
S3rgeyPivovarov <111754932+S3rgeyPivovarov@users.noreply.github.com>
2024-01-10 22:33:04 +0300

    fix: import questions

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const questions = require("./questions");
-
-const body = document.getElementsByTagName("body");
+const body = document.getElementsByTagName("body")[0];
 const container = document.createElement("main");
-const gallows = document.createElement("figure");
-const word = document.createElement("section[class='word']");
-const question = document.createElement("section[class='question']");
-const statusCount = document.createElement("section[class='status']");
-const keyboard = document.createElement("section[class='keyboard']");
+const gallows = document.createElement("section");
+const word = document.createElement("section");
+const question = document.createElement("section");
+const statusCount = document.createElement("section");
+const keyboard = document.createElement("section");
 
 body.className = "page";
 
 container.className = "wrapper";
+container.innerText = "1asda1";
 
 gallows.className = "gallows";
 
@@ -19,3 +18,13 @@ word.className = "word";
 question.className = "question";
 
 statusCount.className = "status";
+
+keyboard.className = "keyboard";
+
+container.appendChild(gallows);
+container.appendChild(word);
+container.appendChild(gallows);
+container.appendChild(question);
+container.appendChild(statusCount);
+container.appendChild(keyboard);
+body.appendChild(container);
